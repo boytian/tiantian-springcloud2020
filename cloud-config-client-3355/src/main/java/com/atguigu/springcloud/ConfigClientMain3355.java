551627e6528cb4975039b6b28e8cfeb0c55603a2
boytian <d1376537549@126.com>
2020-03-11 13:29:02 +0800

    本次提交,配置config，完成动态通知，由于网速原因可能会失败多尝试！

---
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author: TianTian
+ * @Date: 2020/3/11 12:32
+ */
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class);
+    }
+}
